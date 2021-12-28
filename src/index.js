e9c00e38ead91872f4591996d76594bf8688821c
fagnermeds <fagnermeds@gmail.com>
2021-12-28 00:41:51 -0300

    add new router

--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello world!' });
 });
 
+app.get('/names', (request, response) => {
+  return response.json([
+    { id: 1, name: 'fagner' },
+    { id: 2, name: 'emilly' }, 
+    { id: 3, name: 'falconi' }, 
+    { id: 4, name: 'fabricio' }, 
+    { id: 5, name: 'socorro' }, 
+    { id: 6, name: 'francinaldo' },
+  ]);
+})
+
 app.listen(8080, () => console.log('Running in port 8080'));
